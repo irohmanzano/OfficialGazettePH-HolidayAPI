72d11f80cdb9a0165d1b81c2d633dbb3fd51803a
Iroh <manzanoiroh777@gmail.com>
2025-09-02 17:00:06 +0800

    Update user_agent_generator.py

--- user_agent_generator.py
+++ user_agent_generator.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import httpx
+import ssl
+import truststore
+
+ssl_context = truststore.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 
 def get_user_agents():
     url = "https://www.useragentlist.net/"
-    request = httpx.get(url)
+    request = httpx.get(url, verify=ssl_context)
     user_agents = []
     soup = BeautifulSoup(request.text, "html.parser")
     for user_agent in soup.select("pre.wp-block-code"):
